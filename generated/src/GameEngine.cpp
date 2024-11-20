9e5ee6ec91929b3ad1e60c26b099792f9f7b4a85
弗洛林 <75491401+fazy1729@users.noreply.github.com>
2024-11-20 20:52:42 +0200

    UPDATE

--- generated/src/GameEngine.cpp
+++ generated/src/GameEngine.cpp
@@ -10,7 +10,7 @@ GameEngine::GameEngine(const string& fileName) :
     run(true)
 {
     ifstream tempInputFile;
-    tempInputFile.open("C:\\Users\\florin\\Desktop\\Facultate\\An2\\OOP\\scripts\\tastatura.txt");
+    ///tempInputFile.open("C:\\Users\\florin\\Desktop\\Facultate\\An2\\OOP\\scripts\\tastatura.txt");
     if (!tempInputFile) {
         cerr << "Unable to open file " << fileName << endl;
         readingFromFile = false;
