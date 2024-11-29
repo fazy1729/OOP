5e56227732182b17be449b71eae26b1307131b8b
弗洛林 <75491401+fazy1729@users.noreply.github.com>
2024-11-29 18:06:16 +0200

    UPDATED std::

--- generated/src/GameEngine.cpp
+++ generated/src/GameEngine.cpp
@@ -1,7 +1,6 @@
 #include "GameEngine.h"
-using namespace std;
 
-GameEngine::GameEngine(const string& fileName) :
+GameEngine::GameEngine(const std::string& fileName) :
     root("CryptoHacker", "ROOT"),
     cryptoHacker("1"),
     interface(fileName),
@@ -9,108 +8,112 @@ GameEngine::GameEngine(const string& fileName) :
     readingFromFile(true),
     run(true)
 {
-    ifstream tempInputFile;
+    std::ifstream tempInputFile;
     tempInputFile.open(R"(D:\CloneProjectsGIT\proiect_oop\OOP\tastatura.txt)");
     if (!tempInputFile) {
-        cerr << "Unable to open file " << fileName << endl;
+        std::cerr << "Unable to open file " << fileName << std::endl;
         readingFromFile = false;
     } else {
         inputFile = std::move(tempInputFile);
     }
 }
+
 void GameEngine::executeCdCommand() {
-    ///FACEM CD DIN LINUX - EFECTIV NE PLIMBAM PRIN HASHMAP
-    string dir;
-    getline(cin, dir);
-    size_t poz = dir.find(' ');
-    if(poz != string::npos) {
-        dir = dir.substr(poz+1);
+    // FACEM CD DIN LINUX - EFECTIV NE PLIMBAM PRIN HASHMAP
+    std::string dir;
+    std::getline(std::cin, dir);
+    std::size_t poz = dir.find(' ');
+    if(poz != std::string::npos) {
+        dir = dir.substr(poz + 1);
         if(dir != "SECRET")
             terminal.cd(dir);
         else
             level.getPassword(level.getCurrentLevel());
     }
     else
-        cout<<"The correct format is: cd <directory>\n";
-
+        std::cout << "The correct format is: cd <directory>\n";
 }
-void GameEngine::executeCaesarCommand(ifstream &inputFileStream, bool isReadingFromFile) {
-    ///EXECUTAM ALGORITMUL CEZAR
-    string file;
+
+void GameEngine::executeCaesarCommand(std::ifstream &inputFileStream, bool isReadingFromFile) {
+    // EXECUTAM ALGORITMUL CEZAR
+    std::string file;
     if (isReadingFromFile == false)
-        getline(cin,file);
+        std::getline(std::cin, file);
     else {
-        cout<<"You entered: "<< file <<"\n";
-        getline(inputFileStream,file);
+        std::cout << "You entered: " << file << "\n";
+        std::getline(inputFileStream, file);
     }
-    size_t pos = file.find(' ');
-    if(pos != string::npos) {
-        file = file.substr(pos+1);
-        map<string, vector<string> > catFiles = level.getCatFiles();
+    std::size_t pos = file.find(' ');
+    if(pos != std::string::npos) {
+        file = file.substr(pos + 1);
+        std::map<std::string, std::vector<std::string>> catFiles = level.getCatFiles();
         auto it = catFiles.find(file);
         if(it != catFiles.end())
             for(auto &text : it->second)
                 cryptoHacker.CaesarDecypher(text);
         else
-            cout<<"File not found"<<endl;
+            std::cout << "File not found" << std::endl;
     }
     else
-        cout<<"The correct format is: --caesar <file.txt>\n";
+        std::cout << "The correct format is: --caesar <file.txt>\n";
 }
-void GameEngine::executeSHA256Command(ifstream &inputFileStream, bool isReadingFromFile) {
-    ///EXECUTAM ALGORITMUL DE HASHING
-    string fileName;
-    if(isReadingFromFile == false)
-        getline(cin, fileName);
-    else
-        getline(inputFileStream,fileName);
 
-    size_t pos = fileName.find(' ');
-    if(pos != string::npos) {
-        fileName = fileName.substr(pos+1);
-        string hash = cryptoHacker.SHA256(fileName, level.getCatFiles());
-        cout << "SHA256 hash: " << hash << endl;
+void GameEngine::executeSHA256Command(std::ifstream &inputFileStream, bool isReadingFromFile) {
+    // EXECUTAM ALGORITMUL DE HASHING
+    std::string fileName;
+    if(isReadingFromFile == false)
+        std::getline(std::cin, fileName);
+    else
+        std::getline(inputFileStream, fileName);
+
+    std::size_t pos = fileName.find(' ');
+    if(pos != std::string::npos) {
+        fileName = fileName.substr(pos + 1);
+        std::string hash = cryptoHacker.SHA256(fileName, level.getCatFiles());
+        std::cout << "SHA256 hash: " << hash << std::endl;
     }
     else
-        cout<<"The correct format is: --sha256 <file.txt>\n";
+        std::cout << "The correct format is: --sha256 <file.txt>\n";
 }
+
 void GameEngine::executeCATCommand() {
-    ///COMANDA CAT EFECTIV NE ARATA CE SE AFLA IN CHEIA MAPULUI
-    string file;
-    getline(cin,file);
-    terminal.cat(file,level.getCatFiles());
+    // COMANDA CAT EFECTIV NE ARATA CE SE AFLA IN CHEIA MAPULUI
+    std::string file;
+    std::getline(std::cin, file);
+    terminal.cat(file, level.getCatFiles());
 }
-void GameEngine::displayHelp(ifstream &inputFileStream, const bool &isFileRead) {
-    ///PREZENTARE DIVERSE FUNCTII CARE EXISTA PE BAZA CATEGORIEI DIN CARE FAC PARTE
-    cout<<"HOW MIGHT I HELP YOU TODAY?"<<endl;
-    cout<<"1. List of personalities: "<<endl;
-    cout<<"2. List of commands: "<<endl;
-    cout<<"Enter a number:";
+
+void GameEngine::displayHelp(std::ifstream &inputFileStream, const bool &isFileRead) {
+    // PREZENTARE DIVERSE FUNCTII CARE EXISTA PE BAZA CATEGORIEI DIN CARE FAC PARTE
+    std::cout << "HOW MIGHT I HELP YOU TODAY?" << std::endl;
+    std::cout << "1. List of personalities: " << std::endl;
+    std::cout << "2. List of commands: " << std::endl;
+    std::cout << "Enter a number:";
 
     int number;
     if(isFileRead == false)
-        cin>>number;
+        std::cin >> number;
     else {
         inputFileStream >> number;
-        cout<<"You've entered number "<<number<<"\n";
+        std::cout << "You've entered number " << number << "\n";
     }
     if(number == 1) {
-        cout<<"Enter your personality from the list: "<<endl;
+        std::cout << "Enter your personality from the list: " << std::endl;
 
         root.printHackerPersonality();
         int hackerPersonality;
 
-        cout << "Enter a number from the list: ";
+        std::cout << "Enter a number from the list: ";
         if(isFileRead == false)
-            cin>>hackerPersonality;
+            std::cin >> hackerPersonality;
         else {
             inputFileStream >> hackerPersonality;
-            cout<<"You've entered number "<<hackerPersonality<<"\n";
+            std::cout << "You've entered number " << hackerPersonality << "\n";
         }
         if( (hackerPersonality == 1 || hackerPersonality == 2) || hackerPersonality == 3)
             root.help_commands(hackerPersonality);
         else {
-            cout<<"That was a nice try, writing a name that is not in a list... You lost...";
+            std::cout << "That was a nice try, writing a name that is not in a list... You lost...";
             run = false;
         }
     }
@@ -118,70 +121,66 @@ void GameEngine::displayHelp(ifstream &inputFileStream, const bool &isFileRead) 
         terminal.list_commands_help();
 
     else {
-        cout<<"You tried to trick me by inserting something else than a number... Dissapointing...";
+        std::cout << "You tried to trick me by inserting something else than a number... Dissapointing...";
         run = false;
     }
 }
 
-void GameEngine::executeExtractMetadataCommand(ifstream &inputFileStream, bool isReadingFromFile) {
-    string fileName;
+void GameEngine::executeExtractMetadataCommand(std::ifstream &inputFileStream, bool isReadingFromFile) {
+    std::string fileName;
     if (isReadingFromFile)
-        getline(inputFileStream, fileName);
+        std::getline(inputFileStream, fileName);
     else {
-        cout << "Enter the file name: ";
-        getline(cin, fileName);
+        std::cout << "Enter the file name: ";
+        std::getline(std::cin, fileName);
     }
 
-    size_t pos = fileName.find(' ');
-    if (pos != string::npos) {
+    std::size_t pos = fileName.find(' ');
+    if (pos != std::string::npos) {
         fileName = fileName.substr(pos + 1);
         forensicHacker.ExtractMetadata(fileName, level.getCatFiles());
     } else {
-        cout << "The correct format is: --extract-metadata <file.txt>\n";
+        std::cout << "The correct format is: --extract-metadata <file.txt>\n";
     }
 }
-void GameEngine::executeSearchKeywordCommand(ifstream &inputFileStream, bool isReadingFromFile) {
-    string input;
-    string input2;
-    string keyword;
+
+void GameEngine::executeSearchKeywordCommand(std::ifstream &inputFileStream, bool isReadingFromFile) {
+    std::string input;
+    std::string input2;
+    std::string keyword;
     if(isReadingFromFile == false)
-        getline(cin, input);
+        std::getline(std::cin, input);
     else
-        getline(inputFileStream ,input);
-    size_t start = input.find(' ');
-    size_t start2 = input.find(' ', start+1);
-    if (start != string::npos and start2 != string::npos) {
-        input2 = input.substr(start+1, start2-start-1);
-        keyword = input.substr(start2+1);
+        std::getline(inputFileStream ,input);
+    std::size_t start = input.find(' ');
+    std::size_t start2 = input.find(' ', start + 1);
+    if (start != std::string::npos and start2 != std::string::npos) {
+        input2 = input.substr(start + 1, start2 - start - 1);
+        keyword = input.substr(start2 + 1);
         forensicHacker.SearchKeyword(input2, keyword, level.getCatFiles());
     }
     else
-        cout << "The correct format is: --search-keyword <file.txt> <keyword>\n";
+        std::cout << "The correct format is: --search-keyword <file.txt> <keyword>\n";
 }
 
-void GameEngine::executeAnalyzePatternsCommand(ifstream &inputFileStream, bool isReadingFromFile) {
-    string input;
+void GameEngine::executeAnalyzePatternsCommand(std::ifstream &inputFileStream, bool isReadingFromFile) {
+    std::string input;
     if(isReadingFromFile == false)
-        getline(cin, input);
+        std::getline(std::cin, input);
     else
-        getline(inputFileStream ,input);
+        std::getline(inputFileStream ,input);
     // Trim leading spaces
-    size_t startPos = input.find(' ');
-    if (startPos != string::npos) {
+    std::size_t startPos = input.find(' ');
+    if (startPos != std::string::npos) {
         input = input.substr(startPos + 1);
         forensicHacker.AnalyzeFilePatterns(input, level.getCatFiles());
     }
     else
-        cout << "The correct format is: --analyze-patterns <file.txt>\n";
-
+        std::cout << "The correct format is: --analyze-patterns <file.txt>\n";
 }
 
-
-
-
-
-void GameEngine::exec_commands(const string &input) {
-    ///IN FUNCTIE DE INPUT-UL UTILIZATORIULUI EXECUTAM COMANDA, DACA NU EXISTA ATUNCI NE VA AFISA UN MESAJ CORESPUNZATOR
+void GameEngine::exec_commands(const std::string &input) {
+    // IN FUNCTIE DE INPUT-UL UTILIZATORIULUI EXECUTAM COMANDA, DACA NU EXISTA ATUNCI NE VA AFISA UN MESAJ CORESPUNZATOR
     if(input == "ls")
         terminal.ls();
     else if(input == "pwd")
@@ -203,44 +202,44 @@ void GameEngine::exec_commands(const string &input) {
     else if (input == "--analyze-patterns")
         executeAnalyzePatternsCommand(inputFile, readingFromFile);
     else if(input == "--objective")
-        cout<<level.getObjective();
-    else if(input =="--exit")
+        std::cout << level.getObjective();
+    else if(input == "--exit")
         run = false;
     else {
-        cout<<"Command not found... Type --help to see the list of commands..."<<endl;
-        run=false;
+        std::cout << "Command not found... Type --help to see the list of commands..." << std::endl;
+        run = false;
     }
 }
 
 void GameEngine::display_prompt() {
-    ///NE ARATA PROMPTUL
-    string playerName = interface.getPlayerName();
+    // NE ARATA PROMPTUL
+    std::string playerName = interface.getPlayerName();
     terminal.displayPlayerName(playerName);
 }
 
 void GameEngine::readingCommandsFromFile() {
-    string commandLine;
-    while (getline(inputFile, commandLine)) {
+    std::string commandLine;
+    while (std::getline(inputFile, commandLine)) {
         display_prompt();
-        cout << "Executing from file: " << commandLine << endl;
+        std::cout << "Executing from file: " << commandLine << std::endl;
         exec_commands(commandLine);
     }
     readingFromFile = false;
     inputFile.close();
 }
 
-    void GameEngine::start() {
-        while(run) {
-            display_prompt();
-            if(readingFromFile)
-                readingCommandsFromFile();
-            else {
-                string input;
-                cin>>input;
-                exec_commands(input);
-            }
+void GameEngine::start() {
+    while(run) {
+        display_prompt();
+        if(readingFromFile)
+            readingCommandsFromFile();
+        else {
+            std::string input;
+            std::cin >> input;
+            exec_commands(input);
         }
     }
+}
 
 GameEngine::GameEngine(const GameEngine &other)
     : root(other.root),
@@ -253,7 +252,7 @@ GameEngine::GameEngine(const GameEngine &other)
     if (readingFromFile && other.inputFile.is_open()) {
         inputFile.open("tastatura.txt");
         if (!inputFile) {
-            cerr << "Unable to open file tastatura.txt" << endl;
+            std::cerr << "Unable to open file tastatura.txt" << std::endl;
             readingFromFile = false;
         }
     }
@@ -273,11 +272,10 @@ GameEngine& GameEngine::operator=(const GameEngine &other) {
         if (readingFromFile && other.readingFromFile) {
             inputFile.open(R"(D:\CloneProjectsGIT\proiect_oop\OOP\tastatura.txt)");
             if (!inputFile) {
-                cerr << "Unable to open file tastatura.txt" << endl;
+                std::cerr << "Unable to open file tastatura.txt" << std::endl;
                 readingFromFile = false;
             }
         }
     }
     return *this;
 }
-
