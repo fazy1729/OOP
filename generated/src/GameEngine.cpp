7af678fde4264047fc54a7fb0b0697e286c39e8c
弗洛林 <75491401+fazy1729@users.noreply.github.com>
2024-12-31 15:56:22 +0200

    v3

--- generated/src/GameEngine.cpp
+++ generated/src/GameEngine.cpp
@@ -376,7 +376,7 @@ void GameEngine::exec_commands(const std::string &input) {
         else if (input == "--exit")
             run = false;
         else {
-                ///run = false;
+            run = false;
             throw InvalidCommandException(input);
         }
         logger.log("Command executed successfully: " + input);
